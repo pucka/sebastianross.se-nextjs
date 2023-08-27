0797ab1bc7124fcf85d65c076bc9f5023d6c2682
Sebastian Ross <sebbeross@gmail.com>
2023-08-27 12:28:01 +0200

    Fix a11y on slider buttons

--- app/work/ArticleSlider.tsx
+++ app/work/ArticleSlider.tsx
@@ -18,6 +18,7 @@ const NextButton = ({ enabled, onClick }) => {
       )}
       onClick={onClick}
       disabled={!enabled}
+      aria-label="Next image"
     >
       <svg className={styles.embla__button__svg} viewBox="0 0 238.003 238.003">
         <path d="M181.776 107.719L78.705 4.648c-6.198-6.198-16.273-6.198-22.47 0s-6.198 16.273 0 22.47l91.883 91.883-91.883 91.883c-6.198 6.198-6.198 16.273 0 22.47s16.273 6.198 22.47 0l103.071-103.039a15.741 15.741 0 0 0 4.64-11.283c0-4.13-1.526-8.199-4.64-11.313z" />
@@ -39,6 +40,7 @@ const PrevButton = ({ enabled, onClick }) => {
       )}
       onClick={onClick}
       disabled={!enabled}
+      aria-label="Previous image"
     >
       <svg
         className={styles.embla__button__svg}
